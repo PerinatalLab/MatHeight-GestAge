3e23773aa669d6700945366ea05226c24de66007
Jonas B <jonas.bacelis@gmail.com>
2018-09-02 20:59:52 +0200

    explanations for simulation script added

--- 2017/2_interaction_pattern_REPLICATION-USA.R
+++ 2017/2_interaction_pattern_REPLICATION-USA.R
@@ -58,8 +58,8 @@ dat = dat[which((dat$FRACE31==1)&(dat$FBRACE==1)),]
 #### LEAVE ONLY PARITY 0
 #table(dat$PRIORTERM,useNA = "a")
 #table(dat$PRIORLIVE,dat$PRIORDEAD,useNA = "a")
-dat = dat[which((dat$PRIORLIVE==0)&(dat$PRIORDEAD==0)&(dat$PRIORTERM==0)),]
-dim(dat)
+dat1 = dat[which((dat$PRIORLIVE==0)&(dat$PRIORDEAD==0)&(dat$PRIORTERM==0)),]
+dim(dat1)
 
 ### singletons and twins
 table(dat$DPLURAL,useNA="a")
@@ -207,7 +207,9 @@ fun()
 #########
 
 sin = dat[which(dat$status=="singl"),]  # LEAVE ONLY SINGLETONS
-sin = sin[which((!is.na(sin$BVIKTBS))&(!is.na(sin$GRDBS))),]  
+sin = sin[which((!is.na(sin$DBWT))&(!is.na(sin$GRDBS))),]  
+
+
 
 # function estimating gest.Age-adjusted birtweight Z-score  (Marsal et al)
 marsal = function(ga,birthweight,sex){  
@@ -223,8 +225,13 @@ marsal = function(ga,birthweight,sex){
         qnorm(prb)  # z-score
 }
 
+sin$KON = NA
+sin$KON[which(sin$SEX=="M")] = 1
+sin$KON[which(sin$SEX=="F")] = 2
+
+
 # estimate BWzsc (pretending that sex is unknown for all children)
-sin$bw_zsc_Marsal=sapply(1:nrow(sin), function(x) marsal(sin$GRDBS[x],sin$BVIKTBS[x],sex=sin$KON[x]))
+sin$bw_zsc_Marsal=sapply(1:nrow(sin), function(x) marsal(sin$GRDBS[x],sin$DBWT[x],sex=sin$KON[x]))
 head(sin); dim(sin)
 hist(sin$bw_zsc_Marsal)
 
@@ -235,74 +242,81 @@ sin$cat[which(sin$bw_zsc_Marsal>1.5)] = "LGA"
 sin = sin[which(!is.na(sin$cat)),] # remove the "apropriate for gestational age"
 table(sin$cat)
 
+# some corrections
+sin$M_Ht_In[which(sin$M_Ht_In>75)] = 75
+
 ######  polynomial linear regression model
-mod = lm(GRDBS ~ poly(MLANGD,2) * cat, data = sin)
+mod = lm(GRDBS ~ poly(M_Ht_In,2) * cat, data = sin)
+mod = lm(GRDBS ~ M_Ht_In * factor(cat,levels=c("SGA","LGA")), data = sin)  # height in inches
+summary(mod)
+#table(sin$M_Ht_In)
+
 
 fun = function() {
         
-        df = expand.grid(cat=c("LGA","SGA"),MLANGD=140:196)
-        df = df[order(df$cat,df$MLANGD),]
+        df = expand.grid(cat=c("LGA","SGA"),M_Ht_In=56:75)
+        df = df[order(df$cat,df$M_Ht_In),]
         prd = predict(mod,df,interval = "confidence",level = 0.95) # 
         df$nw = prd[,1] # new value (predicted one)
         tst = df$cat=="SGA"
-        plot(df$MLANGD[tst],df$nw[tst],col="cornflowerblue",type="l",ylim=c(min(df$nw)*0.98,max(df$nw)*1.01),
-             ylab="child's gestational age at birth (days)",xlab="maternal height (cm)")
+        plot(df$M_Ht_In[tst],df$nw[tst],col="cornflowerblue",type="l",
+             ylim=c(min(df$nw)*0.98,max(df$nw)*1.01),
+             ylab="child's gestational age at birth (days)",xlab="maternal height (inch)")
         grid()
-        points(df$MLANGD[!tst],df$nw[!tst],col="orange",type="l")
+        points(df$M_Ht_In[!tst],df$nw[!tst],col="orange",type="l")
         #clean the area for legends (white background)
-        polygon(x = c(151,191,191,151,151),y=c(265,265,272,272,265),col = "white",border = NA) 
+        #polygon(x = c(151,191,191,151,151),y=c(265,265,272,272,265),col = "white",border = NA) 
         
         ##### confidence intervals for the model curves
         # for SGA
-        xs = c(df$MLANGD[tst],rev(df$MLANGD[tst]),df$MLANGD[tst][1])
+        xs = c(df$M_Ht_In[tst],rev(df$M_Ht_In[tst]),df$M_Ht_In[tst][1])
         ys = c(prd[tst,2],rev(prd[tst,3]),prd[tst,2][1])
         clrcm = col2rgb("cornflowerblue") / 255 # color components
         ciclr_sga = rgb(clrcm[1,1],clrcm[2,1],clrcm[3,1],0.4) # color for conf. intervals
         polygon(xs,ys,col=ciclr_sga,border = NA)
         # for LGA
-        xs = c(df$MLANGD[!tst],rev(df$MLANGD[!tst]),df$MLANGD[!tst][1])
+        xs = c(df$M_Ht_In[!tst],rev(df$M_Ht_In[!tst]),df$M_Ht_In[!tst][1])
         ys = c(prd[!tst,2],rev(prd[!tst,3]),prd[!tst,2][1])
         clrcm = col2rgb("orange") / 255 # color components
         ciclr_lga = rgb(clrcm[1,1],clrcm[2,1],clrcm[3,1],0.4) # color for conf. intervals
         polygon(xs,ys,col=ciclr_lga,border = NA)
         
         # LEGEND 1
-        x = 152; dx = 5.0; xs = c(x,x+dx,x+dx,x,x)
-        y = 269; dy = 0.5; ys = c(y-dy,y-dy,y+dy,y+dy,y-dy)
+        x = 61; dx = 2; xs = c(x,x+dx,x+dx,x,x)
+        y = 259; dy = 0.5; ys = c(y-dy,y-dy,y+dy,y+dy,y-dy)
         polygon(xs,ys,col=ciclr_sga,border = NA)
         segments(x,y,x+dx,y,col="cornflowerblue") # SGA
         polygon(xs,ys-1.3,col=ciclr_lga,border = NA)
         segments(x,y-1.3,x+dx,y-1.3,col="orange")
-        text(x-2,y+1.5,"model, 95% CI",pos = 4,cex = 0.7)
-        text(x+5,y,"SGA",pos = 4,cex = 0.7)
-        text(x+5,y-1.3,"LGA",pos = 4,cex = 0.7)
+        text(x-2.3,y+1.5,"model, 95% CI",pos = 4,cex = 0.7)
+        text(x+1.5,y,"SGA",pos = 4,cex = 0.7)
+        text(x+1.5,y-1.3,"LGA",pos = 4,cex = 0.7)
         
         ###### data for empirical means
-        sin$matHgh_cat = sin$MLANGD %/% 5 * 5 + 5/2 # more data = smaller bins
-        df_sing = group_by(sin,cat,matHgh_cat) %>% 
+        df_sing = group_by(sin,cat,M_Ht_In) %>% 
                 summarise(n=n(),mnGA=mean(GRDBS),seGA=sd(GRDBS)/sqrt(n)) %>% filter(n>10) %>% ungroup() 
         df_sing = as.data.frame(df_sing)
         df_sing$clr = NA
         df_sing$clr = ifelse(df_sing$cat=="LGA","orange","cornflowerblue")
         df_sing = df_sing[order(df_sing$cat,decreasing = T),]
         # small shift to sseparate LGA and SGA
-        vls = df_sing$matHgh_cat[which(df_sing$cat=="LGA")]
-        df_sing$matHgh_cat[which(df_sing$cat=="LGA")] = vls - 0.2
+        vls = df_sing$M_Ht_In[which(df_sing$cat=="LGA")]
+        df_sing$M_Ht_In[which(df_sing$cat=="LGA")] = vls - 0.2
         
         # data points
-        points(y = df_sing$mnGA, x = df_sing$matHgh_cat,pch=19,col=df_sing$clr, cex=log(df_sing$n,50))
-        points(y = df_sing$mnGA, x = df_sing$matHgh_cat,cex=log(df_sing$n,50)) # "coat"
+        points(y = df_sing$mnGA, x = df_sing$M_Ht_In,pch=19,col=df_sing$clr, cex=log(df_sing$n,50))
+        points(y = df_sing$mnGA, x = df_sing$M_Ht_In,cex=log(df_sing$n,50)) # "coat"
         # 95% Conf.intervals
         df_sing$upp = df_sing$mnGA + 1.96*df_sing$seGA
         df_sing$low = df_sing$mnGA - 1.96*df_sing$seGA
-        segments(x0 = df_sing$matHgh_cat,x1 = df_sing$matHgh_cat,
+        segments(x0 = df_sing$M_Ht_In,x1 = df_sing$M_Ht_In,
                  y0 = df_sing$low,y1=df_sing$upp,lwd=1.5,col="white") # white background
-        segments(x0 = df_sing$matHgh_cat,x1 = df_sing$matHgh_cat,
+        segments(x0 = df_sing$M_Ht_In,x1 = df_sing$M_Ht_In,
                  y0 = df_sing$low,y1=df_sing$upp,lwd=1,col="darkgrey") # line itself
         
         # LEGEND 2
-        x = 168; y = 269; dy = 0.5
-        text(x-5,y+1.5,"mean, 95% CI",pos = 4,cex = 0.7)
+        x = 66; y = 259; dy = 0.5
+        text(x-2,y+1.5,"mean, 95% CI",pos = 4,cex = 0.7)
         points(c(x,x-0.2),c(y,y-1.3),pch=19,cex=1.2,col=c("cornflowerblue","orange"))
         segments(x,y-0.5,x,y+0.8,col="darkgrey") # SGA
         segments(x-0.2,y-1.3-0.8,x-0.2,y-1.3+0.5,col="darkgrey") # LGA
@@ -310,8 +324,8 @@ fun = function() {
         text(x,y-1.3,"LGA",pos = 4,cex = 0.7)
         
         # LEGEND 3
-        x = 179; y = 271
-        text(x-3,y,"observations (n)",pos = 4,cex = 0.7)
+        x = 68.5; y = 261.5
+        text(x-1,y,"observations (n)",pos = 4,cex = 0.7)
         szs = c(1e2,1e3,1e4)
         legend(x,y,legend = szs,pch=1,bty = "n",pt.cex = log(szs,50))
         
--- 2017/simulation_simple.R
+++ 2017/simulation_simple.R
@@ -1,30 +1,43 @@
+# project: "Maternal height - gestational age" study  (aka uterine distention hypothesis)
+# description: a simulation of predicted interaction pattern (GA=MH*UL)
+# method: simulates births occuring due to two competing birth-risks (baseline and interaction)
+# baseline risks - any other (than interaction) genetic and environmental factors that trigger the birth
+# interaction risk - the one due to the space constraint arising from fetal growth and uterine size
+# created: 2017-10-02
+# reviewed: 2018-09-01
+# author: Jonas Bacelis
 
-# maternal height - gestational age study
-# very simple way of simulationg competing birth-risks (baseline and interaction)
-# 2017-10-02
-
-## define helper functions:
+## load packages:
 library(flexsurv)
 library(ggplot2)
-source("~/Dropbox/GIT/SEMFR_HEIGHT-GESTAGE/2017/simulation_functions.R")
 
-# parameters:
-n_ind = 1e4
+## load functions "fun_interactionPlots", "fun_gradHist", "fun_gradHist", "fun_gradHist"
+source("simulation_functions.R") # dir: ~/Dropbox/GIT/SEMFR_HEIGHT-GESTAGE/2017/
+
+## parameters:
+n_ind = 1e4  # sample size
 
 fun_run = function(base_shift,densit,scaPlot,graHistA,graHistB,graHistX) {
-        
-# generate variables
-gas = rgompertz(n_ind*1.1,0.1212,1.026*1e-8) + 150 # natural gest.age-at-birth distribution
+# base_shift = how many days earlier births would be due to baseline risk, as compared to interaction
+# densit = to-plot-or-not the densities (logical TRUE/FALSE)
+# scaPlot = to-plot-or-not main scatterplot (logical TRUE/FALSE) 
+# graHistA = to-plot-or-not final simulated gestational age due to competing baseline and interaction risks (logical TRUE/FALSE) 
+# graHistB = to-plot-or-not counterfactual gestational age due to baseline risks (logical TRUE/FALSE) 
+# graHistX = to-plot-or-not counterfactual gestational age due to interaction (logical TRUE/FALSE) 
+                
+## generate variables
+gas = rgompertz(n_ind*1.1,0.1212,1.026*1e-8) + 150 # approximation of natural gest.age-at-birth distribution
 # truncate the tail (for not wasting precious gradient colors on invisible bars in histogram)
 gas = gas[which(gas>220)]
 gas = sample(gas,n_ind,replace = F)
+
 fgr = rnorm(n_ind,mean=10,sd=1) # fetal growth rate (arbitrary units)
 mus = rnorm(n_ind,mean=10,sd=1) # maternal uterine size (arbitrary units)
 rat =  fgr/mus  # ratio, determining interaction-imposed birth time
 
 # create dependency between maternal height and uterine size
 mhs_src = rnorm(n_ind,mean=165,sd=10) # maternal height (source)
-hlp = mus + rnorm(n_ind)
+hlp = mus + rnorm(n_ind) # helper
 rnk = match(rank(mhs_src),rank(hlp))
 mhs = mhs_src[order(rnk)]; rm(rnk,hlp,mhs_src)
 
@@ -39,7 +52,7 @@ gas_B = sample(gas)  + base_shift
 # create a dataframe 
 dat = data.frame(fgr,mus,mhs,rat,gas_B,gas_X,stringsAsFactors = F)
 
-### determine the actual Gestational age
+### determine the actual Gestational age: whichever risk triggered the birth first
 dat$gas = apply(dat[,c("gas_B","gas_X")],1,min)
 #dat$gas = dat$gas_B
 #dat$gas = dat$gas_X
@@ -80,9 +93,12 @@ mod = lm(gas ~ fgr*mhs,data=dat)
 print(coef(summary(mod)))
 } # end of function
 
+
+
+# run the simulation:
 par(mfrow=c(1,1))
 fun_run(0,F,T,F,F,F) # base_shift,densit,scaPlot,graHistA,graHistB,graHistX
-fun_run(0,F,F,F,T,F) # base_shift,densit,scaPlot,graHistA,graHistB,graHistX
+
         
 
 
@@ -90,7 +106,10 @@ fun_run(0,F,F,F,T,F) # base_shift,densit,scaPlot,graHistA,graHistB,graHistX
 
 
 ####################
-### counterfactual: what if MUS and FGR manifest effect independently
+### counterfactual scenario: 
+### Maternal uterine size and fetal growth rate 
+### manifest their effects independently
+# i.e., what pattern should we expect if there is no physical interaction
 ####################
 
 
@@ -102,8 +121,8 @@ mus = rnorm(n_ind,mean=10,sd=1)
 # rat = ratio  (in this case this is not a fgr/mus ratio at all)
 rat = rep(NA,n_ind)
 rnd = sample(n_ind,n_ind/2,replace = F)
-rat[rnd] = fgr[rnd]
-rat[-rnd] = mus[-rnd]
+rat[rnd] = fgr[rnd] # introduce correlation with fetal growth rate
+rat[-rnd] = mus[-rnd]  # introduce correlation with maternal uterine size
 
 # create dependence between maternal height and uterine size
 mhs_src = rnorm(n_ind,mean=165,sd=10)
@@ -119,9 +138,9 @@ dat$GAintrx = sort(gas)
 dat$GA = apply(dat[,c("GAbase","GAintrx")],1,min)
 # what was the cause of birth?
 dat$cause = NA
-dat$cause[which(dat$GAintrx < dat$GAbase)] = "X"
-dat$cause[which(dat$GAbase < dat$GAintrx)] = "B"
-dat$cause[which(dat$GAintrx == dat$GAbase)] = "N"
+dat$cause[which(dat$GAintrx < dat$GAbase)] = "X" # births due to interaction
+dat$cause[which(dat$GAbase < dat$GAintrx)] = "B" # births due to baseline risks
+dat$cause[which(dat$GAintrx == dat$GAbase)] = "N" # rare event when no particular risk can be blamed
 table(dat$cause)
 
 qnts = quantile(dat$fgr,probs = c(0.1,0.9))
@@ -134,7 +153,7 @@ summary(lm(GA ~ sta*mhs,data=dat))
 
 ggplot(dat, aes(GA, fill = cause)) + geom_histogram(bins=20)
 
-source("~/Dropbox/GIT/SEMFR_HEIGHT-GESTAGE/2017/simulation_functions.R")
+source("simulation_functions.R") # ~/Dropbox/GIT/SEMFR_HEIGHT-GESTAGE/2017/
 mod = lm(GA ~ sta*mhs,data=dat)
 summary(mod)
 fun_interactionPlots(mod,dat$mhs,dat$sta,dat$GA,1)
@@ -146,7 +165,5 @@ summary(lm(rat ~ mus))
 summary(lm(rat ~ fgr + mus))
 summary(lm(rat ~ fgr * mus))
 
-
-##############
 
 
